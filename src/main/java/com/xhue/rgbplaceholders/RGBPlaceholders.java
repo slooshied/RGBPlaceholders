5a490514783ad77bb03f2af7a3bea950cc58e545
BuildTools <unconfigured@null.spigotmc.org>
2021-01-27 01:24:02 -0600

    Initial commit

---
+++ src/main/java/com/xhue/rgbplaceholders/RGBPlaceholders.java
@@ -0,0 +1,17 @@
+package com.xhue.rgbplaceholders;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class RGBPlaceholders extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
