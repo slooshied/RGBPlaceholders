800846613f812a4946916f2d13a388358ce6e701
xHue <70556905+slooshied@users.noreply.github.com>
2021-01-27 01:30:06 -0600

    Update RGBPlaceholders.java

--- src/main/java/com/xhue/rgbplaceholders/RGBPlaceholders.java
+++ src/main/java/com/xhue/rgbplaceholders/RGBPlaceholders.java
@@ -1,5 +1,6 @@
 package com.xhue.rgbplaceholders;
 
+import net.md_5.bungee.api.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class RGBPlaceholders extends JavaPlugin {
@@ -8,10 +9,17 @@ public final class RGBPlaceholders extends JavaPlugin {
     public void onEnable() {
         // Plugin startup logic
 
+
+        ChatColor.of("#123456"); // hex color code
+
+
     }
 
     @Override
     public void onDisable() {
+
+
+
         // Plugin shutdown logic
     }
 }
